9de801440efd5ca5bcf210d592508347c8b7aed0
ziadghalleb <ziad.ghalleb@gitguardian.com>
2021-10-08 16:05:37 +0200

    postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -5,15 +5,3 @@ var pg_user="root";
 var pg_pass="sup3rstr0ngpass1ForGG";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-const secret = "AKIAF6BAFJKR45SAWSZ5"
-
-// import express from "express";
-const express = require("express")
-export default {
-    port,
-    db,
-    cookieSecret: 'AKIAF6BAFJKR45SAWSZ5',
-    cryptoKey: 'a_secure_key_for_crypto_here',
-    cryptoAlgo: 'aes256',
-    hostname: 'localhost'
-}
