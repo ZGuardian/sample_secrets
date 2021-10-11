b40e8a2608ce8843edbc63466cc3ba2abc5e488d
ziadghalleb <ziad.ghalleb@gitguardian.com>
2021-10-11 11:47:28 +0200

    Add: Algolia hardcoded secret

--- postgres_model.js
+++ postgres_model.js
@@ -7,3 +7,6 @@ var pg_pass="sup3rstr0ngpass1ForGG";
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
 
 var mindee_key="fbb000f8917e7abd3c719aa83fe47f26";
+
+var search="2f62be9f6e43a0c82dd510f322ac94db";
+
